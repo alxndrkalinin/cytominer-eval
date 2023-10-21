525beab11ceb1bd13d6a3c7796fc505b472abe76
alxndrkalinin <1107762+alxndrkalinin@users.noreply.github.com>
2023-10-21 17:37:29 -0400

    expose evaluate_metrics instead of evaluate

--- cytominer_eval/__init__.py
+++ cytominer_eval/__init__.py
@@ -1,6 +1,6 @@
 """Calculation of quality metrics for perturbation profiling experiments."""
-from .evaluate import evaluate
+from .evaluate import evaluate_metrics
 from cytominer_eval import __about__
 from cytominer_eval.__about__ import __version__
 
-__all__ = [evaluate, __about__, __version__]
+__all__ = [evaluate_metrics, __about__, __version__]
