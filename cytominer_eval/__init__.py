a9745460c9375068908985f486da42c8cabee4cc
Alexandr Kalinin <1107762+alxndrkalinin@users.noreply.github.com>
2024-02-13 17:23:13 -0500

    chore: expose evaluation of a single metric

--- cytominer_eval/__init__.py
+++ cytominer_eval/__init__.py
@@ -1,6 +1,6 @@
 """Calculation of quality metrics for perturbation profiling experiments."""
-from .evaluate import evaluate_metrics
+from .evaluate import evaluate_metrics, evaluate_metric
 from cytominer_eval import __about__
 from cytominer_eval.__about__ import __version__
 
-__all__ = [evaluate_metrics, __about__, __version__]
+__all__ = [evaluate_metrics, evaluate_metric, __about__, __version__]
